14dfb2cc4e62b06dbcd8b0d38454ee544c29f0c8
girish004 <sabharigirish004@gmail.com>
2021-06-20 21:32:39 +0530

    Login

--- bot/bot.py
+++ bot/bot.py
@@ -2,9 +2,17 @@ import os
 import time
 import sys
 from tqdm import tqdm
-sys.path.append(os.path.join(sys.path[0], "../"))
+import datetime
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import ActionChains
 from instabot import Bot
-bot=Bot()
-bot.login(username="",password="")
-print(bot.api.get_inbox_v2())
-os.system("rmdir /s config")
+driver = webdriver.Chrome(executable_path="C:\\Webdriver\\chromedriver_win32\\chromedriver.exe")
+driver.get("http://www.instagram.com/")
+time.sleep(6)
+logusername=driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input")
+logusername.send_keys("username goes here")
+password=driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input")
+password.send_keys("password goes here")
+driver.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]/button").click()
+driver.close()
